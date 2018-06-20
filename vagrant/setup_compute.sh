7c1a74500ab903e5435f905f25096a1f5be0d0b2
denverwilliams <denver@debian.nz>
2018-06-20 21:48:59 +0000

    KVM doesn't support using lvm for expanding the root fs

--- vagrant/setup_compute.sh
+++ vagrant/setup_compute.sh
@@ -9,9 +9,4 @@ sed -i -e "s/HOSTIP/$ip/" -e "s/HOSTNAME/$host/" local.conf
 ./stack.sh
 
 sudo pvcreate /dev/vdb
-sudo vgextend vagrant-vg /dev/vdb
-sudo lvextend -L +800G /dev/mapper/vagrant--vg-root
-sudo resize2fs /dev/mapper/vagrant--vg-root
-
-sudo pvcreate /dev/vdc
-sudo vgextend stack-volumes-lvmdriver-1 /dev/vdc
+sudo vgextend stack-volumes-lvmdriver-1 /dev/vdb
