3e9d4b14bc8fa060c5700cc25b63dc638fe59f39
denverwilliams <denver@debian.nz>
2018-06-20 09:47:47 +0000

    to support kvm switch to using *v* as the disk prefix

--- vagrant/setup_compute.sh
+++ vagrant/setup_compute.sh
@@ -8,10 +8,10 @@ ip=$(ifconfig | grep 192.168.0 | cut -f2 -d: | cut -f1 -d' ')
 sed -i -e "s/HOSTIP/$ip/" -e "s/HOSTNAME/$host/" local.conf
 ./stack.sh
 
-sudo pvcreate /dev/sdb
-sudo vgextend vagrant-vg /dev/sdb
+sudo pvcreate /dev/vdb
+sudo vgextend vagrant-vg /dev/vdb
 sudo lvextend -L +800G /dev/mapper/vagrant--vg-root
 sudo resize2fs /dev/mapper/vagrant--vg-root
 
-sudo pvcreate /dev/sdc
-sudo vgextend stack-volumes-lvmdriver-1 /dev/sdc
+sudo pvcreate /dev/vdc
+sudo vgextend stack-volumes-lvmdriver-1 /dev/vdc
