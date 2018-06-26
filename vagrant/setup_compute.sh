6a3111c22cf6b46ae6287efa427865def8652911
denverwilliams <denver@debian.nz>
2018-06-26 03:21:40 +0000

    Remove config for addional disks, we don't need this as we are using a bigger base image

--- vagrant/setup_compute.sh
+++ vagrant/setup_compute.sh
@@ -8,5 +8,3 @@ ip=$(ifconfig | grep 192.168.0 | cut -f2 -d: | cut -f1 -d' ')
 sed -i -e "s/HOSTIP/$ip/" -e "s/HOSTNAME/$host/" local.conf
 ./stack.sh
 
-sudo pvcreate /dev/vdb
-sudo vgextend stack-volumes-lvmdriver-1 /dev/vdb
